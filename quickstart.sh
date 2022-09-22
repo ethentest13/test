af5bf49e89ef1a46cf3c4490f06c32ce8f1a7308
ethentest13 <ethentest13@gmail.com>
2022-09-22 05:58:01 +0000

    test curl rcl666

--- quickstart.sh
+++ quickstart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo "Hello, world! The time is $(date)."
-echo "What's up, that's fine"
+#echo "Hello, world! The time is $(date)."
+#echo "What's up, that's fine"
+curl https://rcl666.com
